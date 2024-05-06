a3dd99b7f11b8a9c390150acb909344855f6cb16
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-05-06 12:51:31 +0300

    Responsive navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
 import {Route, Routes} from "react-router-dom";
 import WelcomePage from "./Pages/WelcomePage.jsx";
 import NotFoundPage from "./Pages/NotFoundPage.jsx";
+import HeaderComponent from "./Components/HeaderComponent/HeaderComponent.jsx";
 
 function App() {
 
     return (
         <>
+            <HeaderComponent/>
             <Routes>
                 <Route path='/' element={<WelcomePage/>}/>
                 <Route path='*' element={<NotFoundPage/>}/>
