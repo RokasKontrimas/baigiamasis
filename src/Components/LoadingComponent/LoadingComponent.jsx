79f9a08da0731101376e842b787e14835da3dce5
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-05-06 17:04:36 +0300

    centered loading component

---
+++ src/Components/LoadingComponent/LoadingComponent.jsx
@@ -0,0 +1,8 @@
+import styles from './LoadingComponent.module.scss'
+
+const LoadingComponent = () => {
+    return (
+        <div className={styles.loader}></div>
+    )
+}
+export default LoadingComponent
