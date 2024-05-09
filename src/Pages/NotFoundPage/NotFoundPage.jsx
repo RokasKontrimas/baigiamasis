fb65c887935eff535d5fa29ef7b7bd50cb29e37c
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-05-09 09:03:24 +0300

    Included back link to main page

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import ContainerComponent from "../../Components/ContainerComponent/ContainerComponent.jsx";
+import {Link} from "react-router-dom";
 
 const NotFoundPage = () => {
     return (
-        <div></div>
+        <ContainerComponent>
+            <div style={{textAlign: "center"}}>
+                <h1>
+                    Page not found.
+                    <Link to='/'>Go back.</Link>
+                </h1>
+            </div>
+        </ContainerComponent>
     )
 }
 export default NotFoundPage
